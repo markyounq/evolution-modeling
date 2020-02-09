8d0c89698fd955b50b462488e110fe4c5cb104df
markyounq <mark.young@valpo.edu>
2020-02-09 15:11:09 -0600

    Updated graphs

--- evolution.py
+++ evolution.py
@@ -5,28 +5,35 @@ import math
 import numpy
 from pygame import gfxdraw
 import matplotlib.pyplot as plt
+import statistics as stats
 
 
 def main():
     pygame.init()
     game_width = 800
     game_height = 600
+    
+    #colors
     white = (255,255,255)
     black = (0,0,0)
     red = (255,0,0)
     green = (0,255,0)
     blue = (0,0,255)
-    max_vel = 20
-    fps = 60
+    purple = (102, 0, 102)
+    
+    max_vel = 2
+    fps = 30
     size = 5
     mutation_rate = 0.2
     steering_weights = 0.05
+    vel_steering_weights = 0.5
+    perception_radius_mutation_range = 30
     initial_perception_radius = 100
     boundary_size = 10
     max_vel = 20
     initial_max_force = 0.02
     health = 100
-    max_poison = 50
+    max_poison = 75
     nutrition = [20, -80]
     bots = []
     food = []
@@ -37,8 +44,14 @@ def main():
     clock = pygame.time.Clock()
     
     blue_counts = []
-    yellow_counts = []
+    purple_counts = []
+    blue_speed = []
+    purple_speed = []
     
+    purple_size = []
+    blue_size = []
+    blue_alive = True
+    purple_alive = True
     
     def lerp():
         percent_health = bot.health/health
@@ -59,32 +72,40 @@ def main():
         return(vector)
     
     class create_bot():
-        def __init__(self,x,y,type,dna=False):
+        def __init__(self,x,y,type,max_vel,dna=False):
+            
+            #static traits
+            self.age = 1
             self.type = type
             
-            self.position = numpy.array([x,y],dtype = 'float64')
-            self.velocity = numpy.array([random.uniform(-max_vel,max_vel),random.uniform(-max_vel,max_vel)],dtype = 'float64')
-            self.acceleration = numpy.array([0,0],dtype='float64')
-            
-            #self.color = color
-            #self.health = health
-            self.max_vel = 2
-            self.max_force = 0.5
-            self.size = 5
-            self.age = 1
-            #self.reproduction_rate = reproduction_rate
             if type == 'blue':
                 #blue
                 self.color = blue #(0,0,255)
-                self.health = 75
-                self.reproduction_rate = 0.004
+                self.health = 100
+                self.reproduction_rate = 0.01
+                self.max_vel = 10
+                self.max_age = 100
+                self.size = 3
                 
             else:
-                #yellow
-                self.color = (255,255,0)
-                self.health = 200
-                self.reproduction_rate = 0.002
-                
+                #purple
+                self.color = purple
+                self.health = 375
+                self.reproduction_rate = 0.005
+                self.max_vel = 5
+                self.max_age = 400
+                self.size = 7
+            self.position = numpy.array([x,y],dtype = 'float64')
+            
+            # evolving traits
+            self.max_vel = self.max_vel + random.uniform(-vel_steering_weights, vel_steering_weights)
+            self.velocity = numpy.array([random.uniform(-self.max_vel,self.max_vel),random.uniform(-self.max_vel,self.max_vel)],dtype = 'float64')
+            self.acceleration = numpy.array([0,0],dtype='float64')
+            self.max_force = 0.5
+            self.size = 5
+            
+
+            
             if dna != False:
                 self.dna = []
                 for i in range(len(dna)):
@@ -100,7 +121,7 @@ def main():
                         self.dna.append(dna[i])
             else:
                 self.dna = [random.uniform(-initial_max_force, initial_max_force), random.uniform(-initial_max_force, initial_max_force), random.uniform(0, initial_perception_radius), random.uniform(0, initial_perception_radius)]
-            print(self.dna)
+            #print(self.dna)
             
         def update(self):
             self.velocity += self.acceleration
@@ -108,24 +129,21 @@ def main():
             self.velocity = normalize(self.velocity)*self.max_vel
             
             self.position += self.velocity
-            self.acceleration *= 0    
-            # blue species
-            if self.type == 'blue':
-                self.health -= 0.4
-            # yellow species
-            else:
-                self.health-= 0.2
-            #self.health -= 0.2
+            self.acceleration *= 0  
+            self.health -= self.max_vel/self.size
+
             #self.color = lerp()
             self.health = min(health,self.health)
             
-            if self.age % 1000 == 0:
-                print(self.age,self.dna)
-            self.age +=1
+            
+            if self.age % self.max_age == 0:
+                self.health = 0
+            else:
+                self.age +=1
             
         def reproduce(self):
             if random.random() < self.reproduction_rate:
-                bots.append(create_bot(self.position[0],self.position[1],self.type,self.dna))
+                bots.append(create_bot(self.position[0],self.position[1],self.type,self.max_vel,self.dna))
                 
         def dead(self):
             if self.health > 0:
@@ -144,7 +162,8 @@ def main():
             steering_force = numpy.add(desired_vel,-self.velocity)
             steering_force = normalize(steering_force)*self.max_force
             return(steering_force)
-                
+    
+    
         def eat(self,list_of_stuff,index):
             closest = None
             closest_distance = max(game_width,game_height)
@@ -214,26 +233,30 @@ def main():
             else:
                 return 0,1
             
-    for i in range(5):
-        bots.append(create_bot(random.uniform(0,game_width),random.uniform(0,game_height),'blue')) 
-        bots.append(create_bot(random.uniform(0,game_width),random.uniform(0,game_height),'yellow')) 
+    for i in range(10):
+        bots.append(create_bot(random.uniform(0,game_width),random.uniform(0,game_height),'blue',5)) 
+        bots.append(create_bot(random.uniform(0,game_width),random.uniform(0,game_height),'purple',2)) 
     
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.set_title('Count of Species')
     
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(111)
+    ax2.set_title('Speed of Species')
+    
     
     
     running = True
     while(running):
         game_display.fill(black)
-        if len(bots) < 5 or random.random() < 0.0001:
-            if random.random() > 0.5:
-                bots.append(create_bot(random.uniform(0,game_width),random.uniform(0,game_height),'blue'))
-            else:
-                bots.append(create_bot(random.uniform(0,game_width),random.uniform(0,game_height),'yellow'))
-        if random.random()<0.1:
+        #if len(bots) < 5 or random.random() < 0.0001:
+         #   if random.random() > 0.5:
+          #      bots.append(create_bot(random.uniform(0,game_width),random.uniform(0,game_height),'blue'))
+           # else:
+           #     bots.append(create_bot(random.uniform(0,game_width),random.uniform(0,game_height),'purple'))
+        if random.random()<0.4:
             food.append(numpy.array([random.uniform(boundary_size, game_width-boundary_size), random.uniform(boundary_size, game_height-boundary_size)], dtype='float64'))
         if random.random()<0.01:
             poison.append(numpy.array([random.uniform(boundary_size, game_width-boundary_size), random.uniform(boundary_size, game_height-boundary_size)], dtype='float64'))
@@ -244,27 +267,50 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         blue_count = 0
-        yellow_count = 0
+        purple_count = 0
+        blue_avg_speed = []
+        purple_avg_speed = []
+        
+        blue_avg_size = []
+        purple_avg_size = []
+        
         for bot in bots[::-1]:
             bot.eat(food,0)
             bot.eat(poison,1)
             bot.boundaries()
             bot.update()
-            blues,yellows = bot.count_bot()
+            blues,purples = bot.count_bot()
             blue_count += blues
-            yellow_count += yellows
+            purple_count += purples
             
-            if bot.age > oldest_ever:
-                oldest_ever = bot.age
-                oldest_ever_dna = bot.dna
-                print(oldest_ever,oldest_ever_dna)
-                
+            if bot.type == 'blue':
+                blue_avg_speed.append(bot.max_vel)
+                blue_avg_size.append(bot.size)
+            else:
+                purple_avg_speed.append(bot.max_vel)
+                purple_avg_size.append(bot.size)
+            
+            #if bot.age > oldest_ever:
+             #   oldest_ever = bot.age
+              #  oldest_ever_dna = bot.dna
+               # print(oldest_ever,oldest_ever_dna)
+                #print(bot.max_vel)
             bot.draw_bot()
             if bot.dead():
                 bots.remove(bot)
             else:
                 bot.reproduce()
-                    
+        try:
+            print('Blue Speed',stats.mean(blue_avg_speed))
+
+        except:
+            print('Blue has died out')
+            blue_alive = False
+        try:
+            print('Purple Speed',stats.mean(purple_avg_speed))
+        except:
+            print('Purple has died out')
+            purple_alive = False
         for i in food:
             pygame.draw.circle(game_display,(0,255,0),(int(i[0]),int(i[1])),3)
         for i in poison:
@@ -272,23 +318,43 @@ def main():
         pygame.display.update()
         clock.tick(fps)
         
+        
         blue_counts.append(blue_count)
-        yellow_counts.append(yellow_count)
+        purple_counts.append(purple_count)
         
         line1, = ax.plot(range(1,len(blue_counts)+1), blue_counts, 'blue')
-        line2, = ax.plot(range(1,len(yellow_counts)+1), yellow_counts, 'red')
+        line2, = ax.plot(range(1,len(purple_counts)+1), purple_counts, 'purple')
         #line2, = ax.plot(x, blue_count, 'r-')
         line1.set_ydata(blue_counts)
-        line2.set_ydata(yellow_counts)
+        line2.set_ydata(purple_counts)
         
         line1.set_xdata(range(1,len(blue_counts)+1))
-        line2.set_xdata(range(1,len(yellow_counts)+1))
+        line2.set_xdata(range(1,len(purple_counts)+1))
         
         fig.canvas.draw()
         fig.canvas.flush_events()
         
-        
-        
+        if blue_alive == True and purple_alive == True:
+            blue_speed.append(stats.mean(blue_avg_speed))
+            purple_speed.append(stats.mean(purple_avg_speed))
+            
+            blue_size.append(stats.mean(blue_avg_size))
+            purple_size.append(stats.mean(purple_avg_size))
+            
+            blue_line1, = ax2.plot(blue_speed)
+            #blue_line1, = ax2.plot(range(1,len(blue_speed)+1), blue_speed, 'blue')
+            blue_line1.set_ydata(blue_speed)
+            
+            blue_line1.set_xdata(blue_size)
+            
+            
+            purple_line2, = ax2.plot(purple_speed)
+            #purple_line2, = ax2.plot(range(1,len(purple_speed)+1), purple_speed, 'purple')
+            purple_line2.set_ydata(purple_speed)
+            purple_line2.set_xdata(purple_size)
+            fig2.canvas.draw()
+            fig2.canvas.flush_events()
+            
         
     pygame.quit()
     quit()
